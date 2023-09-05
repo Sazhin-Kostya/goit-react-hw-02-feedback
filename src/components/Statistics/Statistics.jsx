135b90960b3cc6f2737a57c326cec3e0cde06edd
Sazhin-Kostya <sazhinkostya@gmail.com>
2023-09-05 11:37:16 +0300

    Сделал дз

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,26 @@
+import { Statisticss } from './Statistics.styled';
+export function Statistics({ good, bad, neutral, total }) {
+  const countPositiveFeedbackPercentage = () => {
+    return Math.round((good / total) * 100);
+  };
+  return (
+    <Statisticss $total={total}>
+      <h3>Statistics</h3>
+      <span>Good:{good}</span>
+      <br></br>
+      <span>Neutral:{neutral}</span>
+      <br></br>
+      <span>Bad:{bad}</span>
+      <br></br>
+      <span>Total:{total}</span>
+      <br></br>
+      <span>
+        Positive feedback{' '}
+        {countPositiveFeedbackPercentage() > 0
+          ? `${countPositiveFeedbackPercentage()}`
+          : '0'}
+        %
+      </span>
+    </Statisticss>
+  );
+}
