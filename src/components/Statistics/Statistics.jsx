165303cf135ee06ff07ebed783e1a523306dd898
Sazhin-Kostya <sazhinkostya@gmail.com>
2023-09-06 17:21:23 +0300

    Исправил одну ошибку

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -3,6 +3,9 @@ export function Statistics({ good, bad, neutral, total }) {
   const countPositiveFeedbackPercentage = () => {
     return Math.round((good / total) * 100);
   };
+  const totalFeedBack = () => {
+    return good + bad + neutral;
+  };
   return (
     <Statisticss $total={total}>
       <h3>Statistics</h3>
@@ -12,7 +15,7 @@ export function Statistics({ good, bad, neutral, total }) {
       <br></br>
       <span>Bad:{bad}</span>
       <br></br>
-      <span>Total:{total}</span>
+      <span>Total:{totalFeedBack()}</span>
       <br></br>
       <span>
         Positive feedback{' '}
